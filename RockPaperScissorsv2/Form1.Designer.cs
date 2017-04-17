64a612b5cbce9d30c6862cb6dde6b8e802328932
soulxhacker <godotcoffee231@gmail.com>
2017-04-17 19:35:13 +0530

    Initial Commit

---
+++ RockPaperScissorsv2/Form1.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace RockPaperScissorsv2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.musicBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbe = new System.Windows.Forms.Label();
+            this.sd = new System.Windows.Forms.Label();
+            this.YourScore = new System.Windows.Forms.Label();
+            this.CPUScore = new System.Windows.Forms.Label();
+            this.resetBtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.quitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // musicBtn
+            // 
+            resources.ApplyResources(this.musicBtn, "musicBtn");
+            this.musicBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.musicBtn.Name = "musicBtn";
+            this.musicBtn.UseVisualStyleBackColor = false;
+            this.musicBtn.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            this.label1.UseCompatibleTextRendering = true;
+            // 
+            // lbe
+            // 
+            resources.ApplyResources(this.lbe, "lbe");
+            this.lbe.Name = "lbe";
+            // 
+            // sd
+            // 
+            resources.ApplyResources(this.sd, "sd");
+            this.sd.Name = "sd";
+            // 
+            // YourScore
+            // 
+            resources.ApplyResources(this.YourScore, "YourScore");
+            this.YourScore.Name = "YourScore";
+            // 
+            // CPUScore
+            // 
+            resources.ApplyResources(this.CPUScore, "CPUScore");
+            this.CPUScore.Name = "CPUScore";
+            // 
+            // resetBtn
+            // 
+            resources.ApplyResources(this.resetBtn, "resetBtn");
+            this.resetBtn.BackColor = System.Drawing.SystemColors.Highlight;
+            this.resetBtn.Name = "resetBtn";
+            this.resetBtn.UseVisualStyleBackColor = false;
+            this.resetBtn.Click += new System.EventHandler(this.resetBtn_Click);
+            // 
+            // groupBox1
+            // 
+            resources.ApplyResources(this.groupBox1, "groupBox1");
+            this.groupBox1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.groupBox1.Controls.Add(this.sd);
+            this.groupBox1.Controls.Add(this.lbe);
+            this.groupBox1.Controls.Add(this.CPUScore);
+            this.groupBox1.Controls.Add(this.YourScore);
+            this.groupBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.groupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.TabStop = false;
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            resources.ApplyResources(this.fileToolStripMenuItem, "fileToolStripMenuItem");
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.newToolStripMenuItem,
+            this.saveToolStripMenuItem,
+            this.loadToolStripMenuItem,
+            this.quitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            // 
+            // newToolStripMenuItem
+            // 
+            resources.ApplyResources(this.newToolStripMenuItem, "newToolStripMenuItem");
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Click += new System.EventHandler(this.newToolStripMenuItem_Click);
+            // 
+            // saveToolStripMenuItem
+            // 
+            resources.ApplyResources(this.saveToolStripMenuItem, "saveToolStripMenuItem");
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Click += new System.EventHandler(this.saveToolStripMenuItem_Click);
+            // 
+            // loadToolStripMenuItem
+            // 
+            resources.ApplyResources(this.loadToolStripMenuItem, "loadToolStripMenuItem");
+            this.loadToolStripMenuItem.Name = "loadToolStripMenuItem";
+            this.loadToolStripMenuItem.Click += new System.EventHandler(this.loadToolStripMenuItem_Click);
+            // 
+            // quitToolStripMenuItem
+            // 
+            resources.ApplyResources(this.quitToolStripMenuItem, "quitToolStripMenuItem");
+            this.quitToolStripMenuItem.Name = "quitToolStripMenuItem";
+            this.quitToolStripMenuItem.Click += new System.EventHandler(this.quitToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.resetBtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.musicBtn);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button musicBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lbe;
+        private System.Windows.Forms.Label sd;
+        public System.Windows.Forms.Label YourScore;
+        public System.Windows.Forms.Label CPUScore;
+        private System.Windows.Forms.Button resetBtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem quitToolStripMenuItem;
+    }
+}
+
---
+++ RockPaperScissorsv2/Form1.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Media;
+using System.Text.RegularExpressions;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+
+namespace RockPaperScissorsv2
+{
+    public partial class Form1 : Form
+    {
+       
+        List<string> imageLocation = new List<string>();
+   
+        SoundPlayer sp = new SoundPlayer();
+        bool spIsPlaying = true;
+        public Form1()
+        {
+            InitializeComponent();
+            MusicPlay();
+            imagesAdd();
+          
+           
+           
+            
+           
+
+            
+        }
+
+        private void imagesAdd()
+        {
+            imageLocation.Add("../../Resources/RPS pictures/Rock.png");
+            imageLocation.Add("../../Resources/RPS pictures/Paper.png");
+            imageLocation.Add("../../Resources/RPS pictures/Scissor.png");
+        }
+
+        private void MusicPlay()
+        {
+
+
+            sp.SoundLocation = "../../Resources/Music/Music.wav";
+            sp.LoadAsync();
+            sp.PlayLooping();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (spIsPlaying)
+            {
+                Image img = Image.FromFile("../../Resources/RPS pictures/mute.png");
+                musicBtn.BackgroundImage = img;
+                sp.Stop();
+                spIsPlaying = false;
+
+
+            }
+            else {
+                Image img = Image.FromFile("../../Resources/RPS pictures/volume-button-icon-93467.png");
+                musicBtn.BackgroundImage = img;
+                sp.PlayLooping();
+                spIsPlaying = true;
+
+            
+            
+            }
+
+        }
+
+        
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            
+            
+            Random rnd = new Random();
+            int randomNum=rnd.Next(0,3);
+            pictureBox1.ImageLocation=imageLocation[randomNum];
+            pictureBox1.Enabled =false;
+
+            Thread t1 = new Thread(card2Thread);
+            t1.Start();
+          //  t1.Join();
+
+           
+
+
+           
+
+        
+            
+        }
+
+        private void initialImage()
+        {
+            pictureBox1.ImageLocation = "../../Resources/RPS pictures/back.png";
+            pictureBox2.ImageLocation = "../../Resources/RPS pictures/back.png";
+        }
+
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            
+            
+            
+
+        }
+
+
+        private  void result(string card1, string card2)
+        {
+         //   MessageBox.Show(Thread.CurrentContext.ToString());
+
+            if (card1.Equals("Rock") && card2.Equals("Scissor") || card1.Equals("Scissor") && card2.Equals("Paper") || card1.Equals("Paper") && card2.Equals("Rock"))
+            {
+                Invoke(new Action(() => { YourScore.Text = (Convert.ToInt32(YourScore.Text) + 1).ToString(); }));
+
+
+
+            }
+            else if (card1.Equals("Rock") && card2.Equals("Rock") || card1.Equals("Scissor") && card2.Equals("Scissor") || card1.Equals("Paper") && card2.Equals("Paper"))
+            {
+                Invoke(new Action(() => { YourScore.Text = (Convert.ToInt32(YourScore.Text) + 1).ToString(); }));
+                Invoke(new Action(() => { CPUScore.Text = (Convert.ToInt32(CPUScore.Text) + 1).ToString(); }));
+              
+               
+
+            }
+            else
+            {
+                Invoke(new Action(() => { CPUScore.Text = (Convert.ToInt32(CPUScore.Text) + 1).ToString(); }));
+
+            }
+            Thread.Sleep(1000);
+
+            initialImage();
+
+            Invoke(new Action(() => { pictureBox1.Enabled = true; }));
+
+            Invoke(new Action(() => { pictureBox2.Enabled = true; })); 
+          
+        }
+
+        public void card2Thread()
+        {
+            Random rnd = new Random();
+            int randomNum = rnd.Next(0, 3);
+            Thread.Sleep(1000);
+           // label1.Text = "Thinking...";
+            
+            pictureBox2.ImageLocation = imageLocation[randomNum];
+           // label1.Text="";
+            
+
+            string card1 = Regex.Match(pictureBox1.ImageLocation, @"\/([a-zA-Z]+)\.").Groups[1].Value;
+            string card2 = Regex.Match(pictureBox2.ImageLocation, @"\/([a-zA-Z]+)\.").Groups[1].Value;
+
+           result(card1, card2);
+
+
+        }
+
+        private void resetBtn_Click(object sender, EventArgs e)
+        {
+            YourScore.Text = "0";
+            CPUScore.Text = "0";
+        }
+
+        private void newToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            YourScore.Text = "0";
+            CPUScore.Text = "0";
+
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter("../../Resources/RPS pictures/data.txt");
+            sw.WriteLine((YourScore.Text+"\t"+CPUScore.Text));
+            sw.Close();
+           
+        }
+
+        private void quitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void loadToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("../../Resources/RPS pictures/data.txt");
+            string []data=sr.ReadLine().Split('\t');
+            YourScore.Text = data[0];
+            CPUScore.Text = data[1];
+            sr.Close();
+        }
+
+       
+
+       
+    }
+}
